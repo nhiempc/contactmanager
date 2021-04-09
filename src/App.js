e5bb360abb8d2aa403dd598b390d7acd5f0bd71c
nhiempc <nhiem10596@gmail.com>
2021-04-09 15:58:32 +0700

    upload project

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
 import EditContact from './components/contacts/EditContact';
 import {Provider} from './context';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
